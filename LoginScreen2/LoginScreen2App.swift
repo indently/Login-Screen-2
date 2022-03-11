77331b13a35180b5cc301ff36cb8f8b5dc88f6ec
Federico <codepalace@fastmail.se>
2022-03-11 14:20:13 +0100

    Initial Commit

---
+++ LoginScreen2/LoginScreen2App.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreen2App.swift
+//  LoginScreen2
+//
+//  Created by Federico on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
